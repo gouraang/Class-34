2356ba885234348faf4228da6085017659d99349
gouraang <yogitadutta2010@gmail.com>
2020-03-17 16:47:52 +0530

    https://github.com/gouraang/Class-34.git

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 var ball;
-
+var database,position;
 function setup(){
+    database = firebase.database();
+
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballPosition = database.ref('ball/position');
+    ballPosition.on("value", readPosition , showError);
+
 }
 
 function draw(){
@@ -24,6 +29,17 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({
+        'x': position.x + x , 
+        'y': position.y + y
+    })
+   
+}
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.x = position.y;
+}
+function showError(){
+    console.log("error in typing database");
 }
